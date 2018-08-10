31c47ea91349fd05d5a7aebd459ad801c1fbf2aa
Roy <luogao_lg@sina.com>
2018-08-10 18:52:10 +0800

    empty

--- py_generator.py
+++ py_generator.py
@@ -40,7 +40,8 @@ while True:
   except StopIteration as e:
     print('Generator return value:', e.value)
     break
-  
+
+
 # -*- coding: utf-8 -*-
 #Task
 def triangles():
@@ -49,6 +50,7 @@ def triangles():
     yield L
     L = L + [0]
     L = [L[i - 1] + L[i] for i in range(len(L))]
+
 
 # 期待输出:
 # [1]
@@ -64,23 +66,23 @@ def triangles():
 n = 0
 results = []
 for t in triangles():
-    print(t)
-    results.append(t)
-    n = n + 1
-    if n == 10:
-        break
+  print(t)
+  results.append(t)
+  n = n + 1
+  if n == 10:
+    break
 if results == [
-    [1],
-    [1, 1],
-    [1, 2, 1],
-    [1, 3, 3, 1],
-    [1, 4, 6, 4, 1],
-    [1, 5, 10, 10, 5, 1],
-    [1, 6, 15, 20, 15, 6, 1],
-    [1, 7, 21, 35, 35, 21, 7, 1],
-    [1, 8, 28, 56, 70, 56, 28, 8, 1],
-    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+  [1],
+  [1, 1],
+  [1, 2, 1],
+  [1, 3, 3, 1],
+  [1, 4, 6, 4, 1],
+  [1, 5, 10, 10, 5, 1],
+  [1, 6, 15, 20, 15, 6, 1],
+  [1, 7, 21, 35, 35, 21, 7, 1],
+  [1, 8, 28, 56, 70, 56, 28, 8, 1],
+  [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
 ]:
-    print('测试通过!')
+  print('测试通过!')
 else:
-    print('测试失败!')
+  print('测试失败!')
